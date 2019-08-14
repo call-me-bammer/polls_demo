4322b25ba7f69a22b5138ce69c4e69f67cd9edbe
jhcse5189 <jhcse5189@gmail.com>
2019-08-14 18:41:00 +0900

    Django tutorial - Polls and Falls

---
+++ polls/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Question, Choice
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Data information', {'fields': ['pub_date']}),
+    ]
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    inlines = [ChoiceInline]
+
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
